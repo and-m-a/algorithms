37cb199604843351459f3e636b59d2282dad29e0
and-m-a <55346791+and-m-a@users.noreply.github.com>
2022-12-24 15:32:55 +0400

    Create Solution.php

---
+++ DynamicProgramming/LeetCode/Medium/InterleavingString/Solution.php
@@ -0,0 +1,52 @@
+<php
+     
+ class Solution {
+
+    /**
+     * @param String $s1
+     * @param String $s2
+     * @param String $s3
+     * @return Boolean
+     */
+    function isInterleave($s1, $s2, $s3) {
+        
+        $this->s1 = $s1;
+        $this->s2 = $s2;
+        $this->s3 = $s3;        
+
+        return $this->sub(0, 0, 0);
+    }
+
+    function sub(int $i1, int $i2, int $i3) {
+
+        $l1 = $this->s1[$i1];
+        $l2 = $this->s2[$i2];
+        $l3 = $this->s3[$i3];
+
+        if ($l3 === "") {
+            return $l1 === "" && $l2 === "" ? true : false;
+        }
+
+        if (isset($this->cache[$i1][$i2][$i3])) {
+            return $this->cache[$i1][$i2][$i3];
+        }
+
+        if ($l1 && $l3 === $l1) {
+            $sub = $this->sub($i1 + 1, $i2, $i3 + 1);
+
+            if ($sub) {
+                return $this->cache[$i1][$i2][$i3] = true;
+            }
+        }
+
+        if ($l2 && $l3 === $l2) {
+            $sub = $this->sub($i1, $i2 + 1, $i3 + 1);
+
+            if ($sub) {
+                return $this->cache[$i1][$i2][$i3] = true;
+            }
+        }
+
+        return $this->cache[$i1][$i2][$i3] = false;
+    }
+}
